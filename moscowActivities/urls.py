46b7d55c16219f5d8a62182d5d24fa5393a0c04a
hahabuntun <hahabuntun@gmail.com>
2025-10-24 16:17:24 +0300

    Add blank start page

--- moscowActivities/urls.py
+++ moscowActivities/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from rest_framework import routers
+from django.shortcuts import render
 from django.urls import path
+
+def map_view(request):
+    return render(request, 'index.html')
 
 
 urlpatterns = [
+    path("", map_view, name="home"),
     path('admin/', admin.site.urls),
 ]
